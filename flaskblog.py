8c3950ca7a63a7d0c602b1c9189c89e5cf0a6af3
Pranav Sastry <pranava.sri@gmail.com>
2021-04-03 11:13:55 +0530

    Added flaskblog.py

---
+++ flaskblog.py
@@ -0,0 +1,38 @@
+#Author: Pranav Sastry
+#DateTime: 2021-04-02 09:35:00.233711 IST
+
+import sys
+import os
+sys.path.append("/opt/anaconda3/lib/python3.7/site-packages/")
+
+from flask import Flask,render_template,url_for
+from forms import RegistrationForm,LoginForm
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '7606759b9c410f7865f82cd85bf75465'
+
+posts = [{"author":"Pranav Sastry","title":"Hello World from out of this world! From Pranav Sastry!","content":"Crap","date":"23-12-2021","time":"23:33 IST"},
+        {"author":"Pran Sastry","title":"Hello World","content":"Crap","date":"23-12-2021","time":"23:33 IST"},
+        {"author":"Prani Sastry","title":"Hello World","content":"Crap","date":"23-12-2021","time":"23:33 IST"}]
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html',posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html',title="About")
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html',title="Register",form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html',title="Login",form=form)
+
+if __name__=='__main__':
+    app.run(debug=True)
