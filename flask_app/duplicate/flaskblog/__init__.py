fc071ca9b624ab4381d27c1364688dbb54e46156
Pranav Sastry <pranava.sri@gmail.com>
2021-04-07 21:55:12 +0530

    Added __init__.py

---
+++ flask_app/duplicate/flaskblog/__init__.py
@@ -0,0 +1,32 @@
+#Author: Pranav Sastry
+
+import sys
+import os
+sys.path.append("/opt/anaconda3/lib/python3.7/site-packages/")
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flaskblog.config import Config
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.login_view = 'users.login'
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    from flaskblog.users.routes import users
+    from flaskblog.posts.routes import posts
+    from flaskblog.main.routes import main
+    from flaskblog.errors.handlers import errors
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
+    app.register_blueprint(errors)
+    return app
