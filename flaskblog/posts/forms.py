b929bd4539a2c8e3e9fce23fb2d6a7fbb2e2964a
Pranav Sastry <pranava.sri@gmail.com>
2021-04-07 19:15:44 +0530

    Updated forms.py

--- flaskblog/posts/forms.py
+++ flaskblog/posts/forms.py
@@ -3,11 +3,11 @@ from wtforms import StringField,TextAreaField,SubmitField
 from wtforms.validators import DataRequired,Length
 
 class PostForm(FlaskForm):
-    title = StringField('Title',validators=[DataRequired(),Length(min=1,max=30)])
+    title = StringField('Title',validators=[DataRequired(),Length(min=1,max=100)])
     content = TextAreaField('Content',validators=[DataRequired()])
     submit = SubmitField('Post')
 
 class UpdatePostForm(FlaskForm):
-    title = StringField('Title',validators=[DataRequired(),Length(min=1,max=30)])
+    title = StringField('Title',validators=[DataRequired(),Length(min=1,max=100)])
     content = TextAreaField('Content',validators=[DataRequired()])
     submit = SubmitField('Update')
